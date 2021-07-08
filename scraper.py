964e6106ec951c11e5a7c3715dc30234fc250eff
GustavoLara <gcl@lionx.com.br>
2021-07-08 14:16:35 -0300

    2 - Gathered All Pages

--- scraper.py
+++ scraper.py
@@ -2,29 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-prefix = 'https://www.ivalor.com.br'
-webpage = requests.get('https://www.ivalor.com.br/empresas/listagem?p=1').content
-
-soup = BeautifulSoup(webpage, 'html.parser')
-
-
-links = []
-datas = []
-table_body = soup.find(id='tab_empresas_body')
-rows = table_body.findAll('tr')
-for tr in rows:
-    datas.append(tr.find_all('td')[2].find_all('a'))
-
-aLinks = []
-for data in datas:
-    aLinks.append(data[0])
-
-
+count = 0
 bLinks = []
-for x in aLinks:
-    print(x.get('href'))
+for r in range(0, 25):
+    count += 1
+
+    prefix = 'https://www.ivalor.com.br'
+    preurl = 'https://www.ivalor.com.br/empresas/listagem?p='
+    url = preurl + str(count)
+    webpage = requests.get(url).content
+
+    soup = BeautifulSoup(webpage, 'html.parser')
 
 
+    links = []
+    datas = []
+    table_body = soup.find(id='tab_empresas_body')
+    rows = table_body.findAll('tr')
+    for tr in rows:
+        datas.append(tr.find_all('td')[2].find_all('a'))
+
+    aLinks = []
+    for data in datas:
+        aLinks.append(data[0])
+
+
+
+    for x in aLinks:
+        bLinks.append(prefix + x.get('href'))
+
+
+print(bLinks)
 
 
 
